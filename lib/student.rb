a147dc2961f725f6af6874346540badf70feb291
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-23 21:09:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each { |k ,v| self.k = v }
+    attributes_hash.each { |k ,v| self.send "#{k}=", v }
   end
 
   def self.all
